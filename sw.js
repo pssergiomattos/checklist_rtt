14e322968626539f55f01df83f98f6d411861a4b
pssergiomattos <pauloscmat@gmail.com>
2026-09-12 18:14:34 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rtt-check-v18'; // Subimos para v13 para forçar a atualização geral
+const CACHE_NAME = 'rtt-check-v19'; // Subimos para v13 para forçar a atualização geral
 const ASSETS = [
   './',
   './index.html',
